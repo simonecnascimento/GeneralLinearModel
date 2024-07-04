0f86db585777d87608ac3f3b3f60b26a4cc7cf53
simonecnascimento <scarneironascimento@gmail.com>
2024-07-04 16:43:53 -0400

    find matrix location of start and end of event

--- AQuA2_getRisingTime_cellPropagation.m
+++ AQuA2_getRisingTime_cellPropagation.m
@@ -1,4 +1,33 @@
 % i = event number
 for i = 1:numel(res.evtFavList1)
     imagesc(res.riseLst1{1, i}.dlyMap50)
+    
+    %propagation matrix
+    matrix = res.riseLst1{1, i}.dlyMap50;
+
+    % Find the maximum value
+    maxValue = max(matrix(:));
+    % Find all positions of the maximum value
+    [maxRows, maxCols] = find(matrix == maxValue);
+    
+    % Find the minimum value
+    minValue = min(matrix(:));
+    % Find all positions of the minimum value
+    [minRows, minCols] = find(matrix == minValue);
+    
+    % Display the results for maximum value
+    disp(['Maximum value: ', num2str(maxValue)]);
+    disp('Positions of max value:');
+    for j = 1:length(maxRows)
+        disp(['(', num2str(maxRows(j)), ', ', num2str(maxCols(j)), ')']);
+    end
+    
+    % Display the results for minimum value
+    disp(['Minimum value: ', num2str(minValue)]);
+    disp('Positions of min value:');
+    for j = 1:length(minRows)
+        disp(['(', num2str(minRows(j)), ', ', num2str(minCols(j)), ')']);
+    end
+
 end
+
