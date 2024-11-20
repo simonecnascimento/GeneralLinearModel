ebe5e031a6d9126b91c2ecb011e3d68ae072d05f
simonecnascimento <scarneironascimento@gmail.com>
2024-11-20 10:22:06 -0500

    rename

---
+++ AQuA2_featureExtraction_propagation.m
@@ -0,0 +1,45 @@
+%% Extract features of each experiment
+
+% load aqua files (cfuInfo1 and res.featuresTable1)
+
+% define experiment name
+filename = 'Pf4Ai162-20_240229_FOV1_reg_green_Substack(1-927)';
+
+% transform struct to table
+resultsRaw = res.featureTable1;
+resultsExpanded = cell2table(resultsRaw.ftsTb); %expanded events without features column
+       
+% Delete columns = perivascular and multinucleated cells and their respective events
+cols_to_delete = input('Enter columns to delete (e.g., [1,2] ): ');
+
+totalCells = input('Enter total number of remaining cells: ');
+
+% Delete specified columns
+resultsExpanded(:, cols_to_delete) = [];
+
+% propagation speed
+
+% extract propagationDistance and duration of events
+propagationDistance = (table2cell(resultsExpanded(16,:)))';
+duration50to50 = (table2cell(resultsExpanded(9,:)))';
+
+% Initialize the result cell array
+speedEvent = cell(size(propagationDistance));
+
+% Perform element-wise division
+for i = 1:length(propagationDistance)
+    speedEvent{i} = propagationDistance{i} / duration50to50{i};
+end
+
+% save
+newFilename = strcat(filename, '_propagationSpeed.mat');
+save(newFilename);
+
+%
+clear all;
+
+%% Create analysisByEvent
+%Load both _analysis.mat and _propagation.mat of each FOV
+
+Filename_analysisByEvent = strcat(filename, '_analysisByEvent.mat');
+save(Filename_analysisByEvent);
